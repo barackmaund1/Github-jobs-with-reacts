0e7c6213a2b85c52575ab76d5868a850a893e20d
barackmaund1 <pmaundu49@gmail.co>
2020-08-06 02:28:45 +0300

    create pagination

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import useFetchJobs from './useFetchJobs';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import Job from './Job';
+import JobsPaginations from './JobsPaginations';
+import { Prev } from 'react-bootstrap/esm/PageItem';
 
 function App() {
   const [params,setParams] =useState({})
   const [page,setPage]=useState(1)
 
-  const { jobs,loading,error} = useFetchJobs()
+  const { jobs,loading,error,hasNextPage} = useFetchJobs()
+ 
   return (
     <Container className='my-4'>
+      <h1 className='mb-4'>Github Jobs</h1>
+      <JobsPaginations page={page} setPage={setPage} hasNextPage=
+      {hasNextPage}/>
        { loading && <h1>Loading....</h1>}
        { error && <h1>Error.Try Refreshing</h1>}
        {jobs.map(job=>{
          return <Job key={job.id} job={job}/>
        })}
+       <JobsPaginations page={page} setPage={setPage} hasNextPage={hasNextPage}/>
     </Container>
   );
 }
