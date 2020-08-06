84fbdcfca44f88862621b807abc26abc820676d2
barackmaund1 <pmaundu49@gmail.co>
2020-08-06 03:10:42 +0300

    Updates

---
+++ precache-manifest.1ac53565b111ecf61f88271ba56c834f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c27658fd71f3e3a5f60c59e897b161",
+    "url": "/Github-jobs-with-reacts/index.html"
+  },
+  {
+    "revision": "459cfc1280df4133bee4",
+    "url": "/Github-jobs-with-reacts/static/css/main.ea0892d8.chunk.css"
+  },
+  {
+    "revision": "b5319cb4cbaf9d49f4ea",
+    "url": "/Github-jobs-with-reacts/static/js/2.434989e8.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Github-jobs-with-reacts/static/js/2.434989e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459cfc1280df4133bee4",
+    "url": "/Github-jobs-with-reacts/static/js/main.f79b4493.chunk.js"
+  },
+  {
+    "revision": "a51f93a83dad2c27c898",
+    "url": "/Github-jobs-with-reacts/static/js/runtime-main.64e2a373.js"
+  }
+]);
